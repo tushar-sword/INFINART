8369d000f8d40e20d02d776ec01a7f85f9013343
tushar-sword <tushar21kt@gmail.com>
2025-04-21 22:04:10 +0530

    Navbar_tushar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">Infinart</div>
+
+            <ul className="navbar-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/products">Products</a></li>
+        <li><a href="/shops">Shops</a></li>
+        <li><a href="/Blog">Blog</a></li>
+        <li><a href="/contact">Contact Us</a></li>
+
+      </ul>
+
+      <button className='login_button'>Login / Signup</button>
+        </nav>
+    );
+};
+
+export default Navbar;
